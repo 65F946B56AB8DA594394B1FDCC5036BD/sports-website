cc02eaa1839dd3d9c685a17952b348079e3c8f3d
65F946B56AB8DA594394B1FDCC5036BD <149467531+65F946B56AB8DA594394B1FDCC5036BD@users.noreply.github.com>
2023-11-01 12:13:38 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+document.getElementById("toggle-theme").addEventListener("click", function() {
+    document.body.classList.toggle("light-theme");
+});
+// script.js
+document.getElementById("cta-button").addEventListener("click", function() {
+    alert("You clicked the CTA button!");
+  });
+  // script.js
+// You can add JavaScript functionality here if needed.
